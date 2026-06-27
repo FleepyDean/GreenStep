5e25424e1da0aa58e1559cf6b1f82ac76680e8e4
mohamadhafizhaziq-utm <mohamadhafizhaziq@graduate.utm.my>
2026-06-27 20:16:32 +0800

    Add dataset information endpoint

--- api/src/Controllers/AdminController.php
+++ api/src/Controllers/AdminController.php
@@ -6,6 +6,7 @@ namespace App\Controllers;
 use App\Models\Admin;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use App\Services\CarbonDatasetService;
 
 class AdminController
 {
@@ -28,5 +29,18 @@ class AdminController
         return $response
             ->withHeader('Content-Type', 'application/json')
             ->withStatus(200);
+    }
+
+    public function dataset($request, $response)
+    {
+        $info = CarbonDatasetService::getDatasetInfo();
+
+        $response->getBody()->write(json_encode([
+            "success" => true,
+            "data" => $info
+        ]));
+
+        return $response
+            ->withHeader("Content-Type","application/json");
     }
 }
